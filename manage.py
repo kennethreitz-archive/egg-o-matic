1ca0a06e792637dd5bcb9424f0a797cf053e7b3b
Kenneth Reitz <me@kennethreitz.com>
2012-02-02 17:10:17 -0500

    celery

---
+++ manage.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from flask.ext.script import Manager
+from flask.ext.celery import install_commands as install_celery_commands
+
+from myapp import create_app
+
+app = create_app()
+manager = Manager(app)
+
+install_celery_commands(manager)
+
+if __name__ == "__main__":
+    manager.run()
--- yoshi/core.py
+++ yoshi/core.py
@@ -1,10 +1,28 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
 from flask import Flask
+from flask.ext.celery import Celery
+
+
 app = Flask(__name__)
+
+app.config['BROKER_URL'] = os.environ.get('BROKER_URL', 'redis://localhost:6379/0')
+celery = Celery(app)
+
+
+@celery.task(name='yoshi.test_egg')
+def test():
+    import time
+    time.sleep(10)
+    print 'hi!'
+
 
 @app.route('/')
 def hello():
+    test.delay()
     return 'hello world!'
 
 
